0a76c2943c063bb04f2eb0a22baf5703abb56a07
quyenquyen2002 <quyenquyenuet@gmail.com>
2023-12-11 00:11:40 +0700

    7 - FastAPI Git & pre-commit

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+# print((BASE_DIR / "templates").exists())
+
+app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+# REST API -> app to app -> ios to webapp - webapp
+
+
+@app.get("/", response_class=HTMLResponse)  #http GET -> JSON
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "abc": 123
+    })
+
+
+@app.post("/")  # http POST
+def home_detail_view():
+    return {"hello": "world"}
